c193fa791ac4fb808833f025a760c7c614b7743a
risc604 <risc604@ms40.url.com.tw>
2016-04-12 00:21:47 +0800

    modify User Adapter file name

--- app/src/main/java/com/example/tomcat/userlistview/userAdapter.java
+++ app/src/main/java/com/example/tomcat/userlistview/userAdapter.java
@@ -4,11 +4,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.HashMap;
+
 /**
  * Created by tomcat on 2016/4/12.
  */
 public class UserAdapter extends BaseAdapter
 {
+    private String  deviceName;
+    private HashMap<String, Integer>    rssiList;
+
     @Override
     public int getCount()
     {
